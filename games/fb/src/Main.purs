c3e3b79865833d86de9a8eb702c53329a5968a97
yksym <yksym@none>
2018-07-14 13:26:37 +0900

    use only slider

--- games/fb/src/Main.purs
+++ games/fb/src/Main.purs
@@ -1,49 +1,132 @@
 module Main where
 
+import Prelude
+
+import Data.Array (catMaybes)
+import Data.Tuple (Tuple(..))
+import Data.Maybe (Maybe(..), fromMaybe)
+import Data.Number (fromString)
+
+import DOM.HTML.Indexed.InputType (InputType(..)) as I
+import DOM.HTML.Indexed.StepValue (StepValue(..))
+
 import Effect (Effect)
 import Effect.Class (class MonadEffect)
-import Data.Int (toNumber)
-import Node.Path as Path
-import Node.Encoding (Encoding(..))
-import Node.FS.Sync as S
-import Effect.Timer as T
 import Effect.Console (logShow)
---import Effect.Aff (Aff)
-import Prelude
-import Data.Maybe(Maybe(..), fromMaybe)
+import Effect.Timer as T
+
 import Halogen as H
-import DOM.HTML.Indexed.InputType (InputType(..)) as I
---import Halogen.Query.EventSource as ES
+import Halogen.Aff (awaitBody, runHalogenAff)
 import Halogen.HTML as HH
 import Halogen.HTML.Events as HE
 import Halogen.HTML.Properties as HP
-import Halogen.Aff (awaitBody, runHalogenAff)
 import Halogen.VDom.Driver (runUI)
-import Web.UIEvent.MouseEvent (MouseEvent, screenX, screenY)
-import Data.Tuple (Tuple(..))
-import Svg.Elements as SE
-import Svg.Attributes as SA
-import Data.Int (fromString)
+import Data.Lens (Lens', lens, _1, _2, (^.), use, (.=))
 
-type Position = Tuple Number Number
+type F2 = Tuple Number (Tuple Number ( Tuple Number Unit))
 
-type State =
-    { posViewBox :: Position
-    , moveFromViewBox :: Maybe Position
-    , moveFromScreen :: Maybe Position
-    , sliderValue :: Int
+d0 :: Lens' F2 Number
+d0 = _2 <<< _1
+
+d1 :: Lens' F2 Number
+d1 = _2 <<< _1
+
+d2 :: Lens' F2 Number
+d2 = _2 <<< _2 <<< _1
+
+type  State =
+    { kp              :: Number
+    , ki              :: Number
+    , kd              :: Number
+    , a               :: Number
+    , coeff           :: Number
+    , noise           :: Number
+    , input           :: F2
+    , target          :: F2
     }
 
+_input :: Lens' State F2
+_input = lens _.input $ _ {input = _}
+
+initialState :: State
+initialState =
+             { kp : zero
+             , ki : zero
+             , kd : zero
+             , a  : zero
+             , coeff : zero
+             , noise : zero
+             , input : zero
+             , target : zero
+             }
+
 data Query a = Tick a
-             | SliderChange String a
-             | MoveThrough MouseEvent a
-             | MoveStart   MouseEvent a
-             | MoveEnd     MouseEvent a
-             | GetState    (State -> a)
+    | ParamChange  (State -> Number -> State) String a
+    | InputChange  String a
 
-mouseEvent2Pos :: MouseEvent ->  Position
-mouseEvent2Pos me = Tuple (toNumber $ screenX me) (toNumber $ screenY me)
+type SliderProp f =
+    { min       :: Number
+    , max       :: Number
+    , step      :: Number
+    , value     :: Number
+    , handler   :: forall a. Maybe (String -> a -> f a)
+    }
 
+renderSlider :: forall p f. String -> SliderProp f -> HH.HTML p (f Unit)
+renderSlider name prop =
+      HH.div_
+      [
+          HH.label_
+            [ HH.div_ [ HH.text $ name <> " : " <> show prop.value]
+            , HH.input $ 
+                [ HP.type_ I.InputRange
+                , HP.min prop.min
+                , HP.max prop.max
+                , HP.value $ show prop.value
+                , HP.step $ Step prop.step
+                ] <> catMaybes [HE.onValueInput <$> HE.input <$> prop.handler ]
+            ]
+      ]
+
+
+-- lens?
+render :: State -> H.ComponentHTML Query
+render state = 
+  HH.div_
+  [ renderSlider "Kp" {min : -100.0, max : 100.0, step : 1.0, value : state.kp, handler : Just $ ParamChange $ _ {kp = _} }
+  , renderSlider "Ki" {min : -100.0, max : 100.0, step : 1.0, value : state.ki, handler : Just $ ParamChange $ _ {ki = _} }
+  , renderSlider "Kd" {min : -100.0, max : 100.0, step : 1.0, value : state.kd, handler : Just $ ParamChange $ _ {kd = _} }
+  , renderSlider "a"  {min : -100.0, max : 100.0, step : 1.0, value : state.a,  handler : Just $ ParamChange $ _ {a  = _} }
+  , renderSlider "coeff"  {min : -100.0, max : 100.0, step : 1.0, value : state.coeff,  handler : Just $ ParamChange $ _ {coeff = _} }
+  , renderSlider "noise"  {min : -100.0, max : 100.0, step : 1.0, value : state.noise,  handler : Just $ ParamChange $ _ {noise = _} }
+  , renderSlider "input"  {min : -100.0, max : 100.0, step : 1.0, value : state^.(_input <<< d0),  handler : Just $ InputChange}
+  , renderSlider "target" {min : -100.0, max : 100.0, step : 1.0, value : state.target^.d0, handler : Nothing}
+  ]
+
+eval :: forall m. (MonadEffect m) => Query ~> H.ComponentDSL State Query Void m
+eval = case _ of
+        ParamChange update s next -> fromMaybe (pure next) $ do
+            x <- fromString s
+            pure $ do
+                H.liftEffect $ logShow $ show x
+                void $ H.modify $ (\a b -> update b a) x
+                pure next
+        InputChange s next ->  fromMaybe (pure next) $ do
+            x <- fromString s
+            pure $ do
+                input0 <- use $ _input
+                _input <<< d0 .= x
+                _input <<< d1 .= (x - (input0 ^. d0))
+                _input <<< d2 .= (x - (input0 ^. d0)) - (input0 ^. d1)
+                pure next
+        Tick next -> do
+          void $ H.modify $ \st -> st {
+              target = rungeKutta st
+          }
+          pure next
+
+rungeKutta :: State -> F2
+rungeKutta _ = zero
 
 component :: forall m. (MonadEffect m) => H.Component HH.HTML Query Unit Void m
 component =
@@ -53,102 +136,12 @@ component =
     , eval
     , receiver: const Nothing
     }
-  where
-
-  initialState :: State
-  initialState =
-               { posViewBox      : zero
-               , moveFromViewBox : Nothing
-               , moveFromScreen  : Nothing
-               , sliderValue     : 3
-               }
-
-  render :: State -> H.ComponentHTML Query
-  render state = 
-    HH.div_
-    [
-        HH.label_
-          [ HH.div_ [ HH.text $ "value:" <> show state.sliderValue]
-          , HH.input
-              [ HP.type_ I.InputRange
-              , HP.min 1.0
-              , HP.max 10.0
-              , HP.value "3.0"
-              -- , HP.class_ $ ClassName "slider"
-              , HE.onValueInput (HE.input SliderChange)
-              ]
-          ]
-    ,   SE.svg [SA.width w, SA.height h, SA.viewBox x y w h]
-        [ SE.circle
-          [ SA.r r
-          , SA.fill $ Just (SA.RGB 0 0 100)
-          , HE.onMouseDown (HE.input MoveStart)
-          , HE.onMouseUp   (HE.input MoveEnd)
-          , HE.onMouseLeave(HE.input MoveEnd)
-          , HE.onMouseMove (HE.input MoveThrough)
-          ]
-        ]
-    ]
-
-    where
-    h = 150.0
-    w = 150.0
-    r = w / 6.0
-    Tuple x y = state.posViewBox - (Tuple (w / 2.0) (h / 2.0))
-
-  eval :: (MonadEffect m) => Query ~> H.ComponentDSL State Query Void m
-  eval = case _ of
-          SliderChange v next -> do
-            _ <- H.modify $ \state -> state { sliderValue = fromMaybe 0 $ fromString v }
-            H.liftEffect $ logShow $ "!!!" <> v
-            pure next
-          Tick next -> do
-            H.liftEffect $ logShow $ "tick"
-            pure next
-          MoveStart me next -> do
-            _ <- H.modify $ \state -> state
-                { moveFromViewBox = Just state.posViewBox
-                , moveFromScreen = Just $ mouseEvent2Pos me
-                }
-            --H.liftEffect $ logShow $ "move start: " <> show (mouseEvent2Pos me)
-            pure next
-          MoveEnd me next -> do
-            _ <- H.modify $ \state -> state
-                { moveFromViewBox = Nothing
-                , moveFromScreen = Nothing
-                }
-            --H.liftEffect $ logShow $ "move end"
-            pure next
-          MoveThrough me next -> do
-            s <- H.get
-            case s.moveFromScreen of
-                Just offsetS -> do
-                    --H.liftEffect $ logShow $ mouseEvent2Pos me
-                    _ <- H.modify $ \state -> state
-                        { posViewBox = fromMaybe state.posViewBox $ do
-                            offsetVB <- state.moveFromViewBox
-                            pure $ offsetVB - (mouseEvent2Pos me - offsetS)
-                        }
-                    --s' <- H.get
-                    --H.liftEffect $ logShow $ s'.posViewBox
-                    pure next
-                Nothing -> do
-                    pure next
-          GetState reply -> do
-            s <- H.get
-            pure $ reply s
-
 
 
 main :: Effect Unit
 main = runHalogenAff do
   body <- awaitBody
   io <- runUI component unit body
-  void $ H.liftEffect $ T.setInterval 2000 $ do
+  void $ H.liftEffect $ T.setInterval 1000 $ do
     runHalogenAff $ io.query $ H.action $ Tick
-  void $ H.liftEffect $ T.setInterval 2000 $ do
-    runHalogenAff $ do
-        state <- io.query $ H.request $ GetState
-        H.liftEffect $ S.appendTextFile ASCII (Path.concat [".", "hoge.txt"]) $ show state <> "\n"
-
 
